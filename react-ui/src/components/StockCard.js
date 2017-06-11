8d2b182dfd25c61f2b306b6c3cb9b1e2a1d5a23b
Bill Hefty <billhefty@gmail.com>
2017-06-11 16:07:07 -0500

    add init StockCard component

---
+++ react-ui/src/components/StockCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const StockCard = props => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  )
+}
+
+StockCard.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
+export default StockCard
