eebbc3878307a4409e54ffeb25016a439b5a41af
Bill Hefty <billhefty@gmail.com>
2017-06-11 11:40:32 -0500

    add testing for fetchStockData

---
+++ tests/utils/fetchStockData.test.js
@@ -0,0 +1,63 @@
+/* eslint-env mocha */
+require('dotenv').config()
+const expect = require('chai').expect
+const nock = require('nock')
+const fetchStockData = require('../../utils/fetchStockData')
+
+const API_URI = process.env.QUANDL_API_URI
+const API_KEY = process.env.QUANDL_API_KEY
+
+describe('fetchStockData', () => {
+  // Create high order function to handle async calls
+  // http://staxmanade.com/2015/11/testing-asyncronous-code-with-mochajs-and-es7-async-await/
+  let mochaAsync = (fn) => {
+    return async () => {
+      try {
+        await fn()
+      } catch (err) {
+        throw new Error(err)
+      }
+    }
+  }
+
+  beforeEach(done => {
+    const fetchResponse = {
+      dataset_data: {
+        data: [
+          ['2017-06-09', 52.60],
+          ['2017-06-08', 51.40],
+          ['2017-06-07', 53.00]
+        ]
+      }
+    }
+
+    const fetchError = { message: 'You have submitted an incorrect company symbol.' }
+
+    nock(API_URI)
+      .get(`/GOOGL/data.json?api_key=${API_KEY}&start_date=2016-06-02&column_index=1`)
+      .reply(200, fetchResponse)
+
+    nock(API_URI)
+      .get(`/MT/data.json?api_key=${API_KEY}&start_date=2016-06-02&column_index=1`)
+      .reply(404, fetchError)
+
+    done()
+  })
+
+  it('should return stock data for given company symbol', mochaAsync(async () => {
+    // Expected data becomes reversed from the respose within fetchStockData
+    const expectedData = [
+      [Date.parse('2017-06-07'), 53.00],
+      [Date.parse('2017-06-08'), 51.40],
+      [Date.parse('2017-06-09'), 52.60]
+    ]
+    const data = await fetchStockData('GOOGL')
+    expect(data).to.eql(expectedData)
+  }))
+
+  it('should return an error message if an incorrect symbol is given', mochaAsync(async () => {
+    const expectedResponse = { message: 'You have submitted an incorrect company symbol.' }
+    const response = await fetchStockData('MT')
+    expect(response.message).to.equal(expectedResponse.message)
+  }))
+})
--- utils/fetchStockData.js
+++ utils/fetchStockData.js
@@ -5,12 +5,19 @@ const API_URI = process.env.QUANDL_API_URI
 const API_KEY = process.env.QUANDL_API_KEY
 
 const fetchStockData = async (symbol) => {
-  const res = await fetch(`${API_URI}/${symbol}/data.json?api_key=${API_KEY}&start_date=2016-06-02&column_index=1`)
-  const stock = await res.json()
-  const stockData = stock.dataset_data.data.map(data => {
-    return [Date.parse(data[0]), data[1]]
-  }).reverse()
-  return stockData
+  try {
+    const res = await fetch(`${API_URI}/${symbol}/data.json?api_key=${API_KEY}&start_date=2016-06-02&column_index=1`)
+    if (res.status >= 400) {
+      throw new Error('Bad response from server')
+    }
+    const stock = await res.json()
+    const stockData = stock.dataset_data.data.map(data => {
+      return [Date.parse(data[0]), data[1]]
+    }).reverse()
+    return stockData
+  } catch (e) {
+    return { message: 'You have submitted an incorrect company symbol.' }
+  }
 }
 
 module.exports = fetchStockData
