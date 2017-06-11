993f5721a63ac6df00aaaad250ed4592538cff11
Bill Hefty <billhefty@gmail.com>
2017-06-10 20:20:19 -0500

    Fix testing to work with highcharts

--- react-ui/src/App.test.js
+++ react-ui/src/App.test.js
@@ -1,9 +1,8 @@
 /* eslint-env jest */
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
+import { shallow } from 'enzyme'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  expect(shallow(<App />).exists()).toBeTruthy()
 })
